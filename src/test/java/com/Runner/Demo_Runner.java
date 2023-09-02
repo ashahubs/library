9d5487773ea2d4c163e02a08eb4214238f524c4e
äshahubs <asha.bishoyi@gmail.com>
2023-09-02 14:49:44 +0530

    Recent Code was updated

--- src/test/java/com/Runner/Demo_Runner.java
+++ src/test/java/com/Runner/Demo_Runner.java
@@ -2,4 +2,19 @@ package com.Runner;
 
 public class Demo_Runner {
 
+	private void helo() {
+System.out.println("Meena");
+	}
+	
+	
+	private void hi() {
+System.out.println("Äasha");
+	}
+	
+	public static void main(String[] args) {
+		Demo_Runner d = new Demo_Runner();
+		d.helo();
+		d.hi();
+	}
+	
 }
